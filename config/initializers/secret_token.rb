625f5ccf2e2ff7b164c9698ecf93c8c3a4a5fa4c
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2015-03-17 14:12:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibwhereyApi::Application.config.secret_key_base = '1d320239c704a1c1a8b0a39e323192c34b68ed943f8ea528538dc381b69dfa444e21df36eea435064808f8b46c54dabceb828788a4e2821034b67ca67cd073df'
